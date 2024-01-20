e14c4a4dcbba1054a67626d14148eebfa233ff47
ncalihu0 <nickycalihua28@gmail.com>
2024-01-19 19:33:39 -0500

    added regex validation

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import validator from 'validator';
 
 export default function Signup() {
     const navigate = useNavigate();
@@ -12,21 +13,30 @@ export default function Signup() {
             mail: mail.value,
             password: password.value
         }
+        if (!validator.isEmail(details.mail)) {
+            return alert('Please input valid email!')
+        } else if (!/^[a-zA-Z0-9]+$/g.test(details.username)) {
+            return alert('Invalid username. Please use alphanumeric characters only.')
+        } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/g.test(details.password)) {
+            return alert('Password must have at least 8 characters, including at least one uppercase letter, one lowercase letter, and one number')
+        } else {
+            try {
+                let res = await axios.post("http://localhost:8080/signUp", details);
+                let result = await res.data
+                if (result.message) {
+                    alert(result.message)
+                    navigate('/');
 
-        try {
-            let res = await axios.post("http://localhost:8080/signUp", details);
-            let result = await res.data
-            if (result.message) {
-                alert(result.message)
-                navigate('/');
+                } else {
+                    alert(`User couldn't be created`)
+                }
 
-            } else {
-                alert(`User couldn't be created`)
+            } catch (err) {
+                alert("error")
             }
-
-        } catch (err) {
-            alert("error")
         }
+
+
 
     }
     return (
